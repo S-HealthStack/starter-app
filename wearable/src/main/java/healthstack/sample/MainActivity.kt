641610e055f8595c7cfb95dfe3e49176d96c06ce
Seongjae Choi <cubist.choi@samsung.com>
2024-06-19 18:34:09 +0900

    Enumize ECG

--- wearable/src/main/java/healthstack/sample/MainActivity.kt
+++ wearable/src/main/java/healthstack/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
+import healthstack.common.model.PrivDataType.ECG
 import healthstack.wearable.support.BaseActivity
 
 @AndroidEntryPoint
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val healthDataTypes = listOf("ECG")
+        val healthDataTypes = listOf(ECG)
 
         setContent {
             BaseActivity(
